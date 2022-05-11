89bc1056c098ff66e1f5c4ec68c512d3d028bca1
Adwait Gharpure <adwaitgharpure@gmail.com>
2022-05-12 02:45:37 +0530

    added circular classes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+// import bgimg from './images/bgimg.png';
+import homeJamLogo from './images/homeJamLogo.png';
+import {HiOutlineSearch} from 'react-icons/hi';
+import {FiShoppingBag} from 'react-icons/fi';
+import {BsSuitHeart} from 'react-icons/bs';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    {/* <div> <img src = {bgimg}></img> */}
+    <div className='container-navbar'>
+      <img className='homeJamLogo'src={homeJamLogo} alt = 'logo'/>
+      <div className='logo'>
+        <div><HiOutlineSearch size={".8rem"}/>  Search </div> 
+        <div> Help </div> 
+        <div> Account </div> 
+        <div><FiShoppingBag size = {".8rem"}></FiShoppingBag></div>
+      </div>
     </div>
+    <div className='header-title'>
+      Cari Cari
+    </div>
+    <div className='header-body'>
+      Live from their sofa to yours. Get closer to your favorite artists, and never miss out
+    </div>
+    <div class="circle-container">
+      <div class='circle' font = 'Nunito' color='white' > Label1</div>
+      <div class='circle' font = 'Nunito' color='white' >Label2</div>
+      <div class='circle' font = 'Nunito' color='white' >Label3</div>
+      <div class='circle' font = 'Nunito' color='white' >Label4</div>
+   </div>
+    
+    </>
   );
-}
+};
 
 export default App;
