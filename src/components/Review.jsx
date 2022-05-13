58c2188ee311dbad65de8dbda5ec2b70620b69bf
Adwait Gharpure <adwaitgharpure@gmail.com>
2022-05-13 16:22:27 +0530

    added alt text to pictures to remove warnings

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -6,7 +6,7 @@ const Review = (props) => {
       <div className="review-card">
       <div className='profile'>
           <div>
-              <img src = {props.image}></img>
+              <img src = {props.image} alt = 'display-picture'></img>
           </div>
           <div className='namecon'>
               <div className='profile-name'>{props.name}</div>
